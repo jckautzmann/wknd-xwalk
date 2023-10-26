08d2cb49c1a2d6d3965dd803cb496a8ef57e2dcf
jckautzmann <jkautzma@adobe.com>
2023-10-26 19:52:32 +0200

    open with Universal Editor

---
+++ tools/sidekick/ue/index.js
@@ -0,0 +1,5 @@
+(async () => {
+  const url = 'https://author-p15404-e146221-cmstg.adobeaemcloud.com/bin/wcmcommand?cmd=open&_charset_=utf-8&path=/content/test-jck-1/index';
+  // open this url in a new tab
+  window.open(url, '_blank');
+})();
