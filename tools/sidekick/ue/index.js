9184f558102346b905c00ce8043ac100ade736c5
jckautzmann <jkautzma@adobe.com>
2023-10-26 20:20:26 +0200

    commit

--- tools/sidekick/ue/index.js
+++ tools/sidekick/ue/index.js
@@ -1,5 +1,5 @@
 (async () => {
   const url = 'https://author-p15404-e146221-cmstg.adobeaemcloud.com/bin/wcmcommand?cmd=open&_charset_=utf-8&path=/content/test-jck-1/index';
-  // open this url in a new tab
-  window.open(url, '_blank');
+  // open this url in the same tab
+  window.open(url, '_self');
 })();
